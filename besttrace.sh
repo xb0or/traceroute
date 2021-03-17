a63cedce4c82176fa86da1c8c24c7b4900cb1812
xb0or <57210034+xb0or@users.noreply.github.com>
2021-03-17 11:14:56 +0800

    Add files via upload

---
+++ besttrace.sh
@@ -0,0 +1,9 @@
+ echo "回程测试 1.3.1 (03/12/2021)"
+
+	read -p "请输入要测回程的IP : " ip_list
+	wget https://github.com/xb0or/traceroute/raw/main/besttrace
+	chmod +x besttrace
+	clear
+	printf "%-70s\n" "-" | sed 's/\s/-/g'
+	./besttrace -q 1 ${ip_list}
+	printf "%-70s\n" "-" | sed 's/\s/-/g'
